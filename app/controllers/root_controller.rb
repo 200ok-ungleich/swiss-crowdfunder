97572a21122850df698e947ae1dcc6f077040bf6
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-09-21 17:50:37 +0200

    Create Root Page featuring current campaigns

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,13 @@
+class RootController < ApplicationController
+  before_action :set_campaigns, only: [:index]
+  def index
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_campaigns
+
+      @campaigns = Campaign.all.limit(10)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :campaigns, only: [:show] do
     resources :goodies, only: [:index]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "root#index"
 end
