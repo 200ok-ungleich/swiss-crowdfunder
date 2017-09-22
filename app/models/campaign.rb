4db479784d87729683f956d80b1d9cd928ab7662
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-09-22 09:33:25 +0200

    Spec + Bugfix

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,7 +4,9 @@ class Campaign < ApplicationRecord
   before_save :use_youtube_embedd_url
 
   def amount_raised
-    goodies.reduce(0) { |sum, g| g.orders.sum(&:amount) if g.orders }
+    goodies.inject(0) do |sum, g|
+      sum += g.orders ? g.orders.sum(&:amount) : sum
+    end
   end
 
   private
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe Campaign, type: :model do
       goodie.orders.create! payment_type: "stripe", quantity: 1, amount: 123
       expect(campaign.amount_raised).to eq(123)
     end
+
+    it "works for multiple goodies/orders" do
+      campaign = Campaign.create! title: "foo"
+      goodie1 = Goodie.create! title: "goodie1", campaign: campaign
+      goodie2 = Goodie.create! title: "goodie2", campaign: campaign
+      goodie3 = Goodie.create! title: "goodie3", campaign: campaign
+      goodie1.orders.create! payment_type: "stripe", quantity: 1, amount: 10
+      # goodie2 has no orders
+      goodie3.orders.create! payment_type: "stripe", quantity: 1, amount: 30
+      goodie3.orders.create! payment_type: "stripe", quantity: 1, amount: 40
+      expect(campaign.amount_raised).to eq(80)
+    end
   end
 
 end
