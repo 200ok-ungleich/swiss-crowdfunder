e912b7243b86702875df2bf023d0eeeae5a3f953
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-04 19:39:09 +0200

    Dependent destroy Supporter when destroying Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :goody
+  has_one :supporter, dependent: :destroy
 
   validates :payment_type, inclusion: { in: %w(stripe bank) }
   validates :agreement, presence: true
