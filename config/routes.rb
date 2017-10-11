2eb83c2e33e3b1b0f9744a0f8198dc61877df7c1
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-11 20:39:18 +0200

    Implement language selection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :campaigns, only: [:show] do
     resources :goodies, only: [:index] do
       resources :orders, only: [:new, :show, :create]
