d1eff41ba89dfd3532bfe4204027000e654c9579
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-20 00:44:58 +0200

    Add tests for About-Us and TOS pages

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe "layout application", :type => :feature do
-  pending "" do
-    raise Error
+  it "shows the about page" do
+    visit root_path
+    find('.qa-about-us a').click
+    expect(page.status_code).to eq(200)
+    expect(current_path).to eq(static_about_us_path)
   end
+
+  it "shows the tos page" do
+    visit root_path
+    find('.qa-terms-of-service a').click
+    expect(page.status_code).to eq(200)
+    expect(current_path).to eq(static_terms_of_service_path)
+  end
+
 end
