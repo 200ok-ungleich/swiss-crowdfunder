9cbde289065fe389481a98430ff04f3014385576
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-23 20:26:52 +0200

    Remove image column from goodies

--- app/admin/goody.rb
+++ app/admin/goody.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Goody do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :title, :description, :image, :price, :quantity, :campaign_id
+  permit_params :title, :description, :price, :quantity, :campaign_id
 
 #
 # or
