6751497d41a983d6c3993c8c1854ccaf9bf81e50
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-24 22:00:46 +0200

    Introduce FactoryBot to spec/features/admin_spec.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
 
   factory :campaign do
     sequence(:title) { |n| "Campaign #{n}" }
-    description "bar"
+    description "Spec Campaign"
+    claim 'Spec Claim'
+    start_date 10.days.before
+    end_date 40.days.from_now
+    youtube_url 'https://www.youtube.com/watch?v=ZUgjm-XqKuc'
+    goal 1000
   end
 
   factory :goody do
@@ -14,5 +19,11 @@ FactoryBot.define do
     sequence(:first_name) { |n| "John, the #{n}" }
     last_name  "Doe"
     date_of_birth { 18.years.ago }
+  end
+
+  factory :admin_user do
+    sequence(:email) { |n| "admin#{n}@example.com" }
+    password "123123123"
+    password_confirmation "123123123"
   end
 end
