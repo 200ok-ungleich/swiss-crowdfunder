df827b6e25f18f757dea442d748c061ec93a95e1
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-24 22:49:35 +0200

    Admin Dashboard: Supporter Logic

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -5,8 +5,13 @@ class Supporter < ApplicationRecord
   validate :date_of_birth, :is_old_enough?
 
   def is_old_enough?
+    unless date_of_birth.is_a?(Date)
+      errors.add(:date_of_birth, I18n.t("supporter.validation.is_not_a_date"))
+      return
+    end
     unless Date.today.year - self.date_of_birth.year >= 18
       errors.add(:date_of_birth, I18n.t("supporter.validation.not_old_enough"))
     end
   end
+
 end
