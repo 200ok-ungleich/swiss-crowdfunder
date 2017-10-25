40c001d6063f8410d8d33b7a264adf3d49801cb2
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-25 15:33:08 +0200

    campaigns#show: fix regression on progress bar

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,9 @@
 module CampaignsHelper
+
+  def campaign_percentage(campaign)
+    number_to_percentage(100*(campaign.amount_raised.to_f / campaign.goal),
+                         precision: 1,
+                         separator: '.')
+    .delete(' ') # In some localizations there's whitespace between the number and the percent
+  end
 end
