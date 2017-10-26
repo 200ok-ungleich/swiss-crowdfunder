c5a50ec3eee14eaf464c0731ec161f7aacb08564
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-26 16:30:55 +0200

    Integrate database cleaner

--- Gemfile
+++ Gemfile
@@ -79,6 +79,10 @@ group :production, :test do
   gem 'pg'
 end
 
+group :test do
+  gem 'database_cleaner'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
 
-  describe "Amount raised" do
-    it "works for zero orders" do
+  describe 'Amount raised' do
+    it 'works for zero orders' do
       campaign = FactoryBot.create :campaign
       goody = FactoryBot.create :goody
       expect(campaign.amount_raised).to eq(0)
     end
 
-    it "works for one goody/one order" do
+    it 'works for one goody/one order' do
       campaign = FactoryBot.create :campaign
       goody = FactoryBot.create :goody, campaign: campaign
       supporter = FactoryBot.build :supporter
-      goody.orders.create! payment_type: "stripe",
+      goody.orders.create! payment_type: 'stripe',
         quantity: 1,
         amount: 123,
         agreement: true,
