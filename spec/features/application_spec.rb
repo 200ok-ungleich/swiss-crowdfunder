1000b66d41fce8bb1ee48e21612865f4c3b4053a
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-10-29 23:03:46 +0100

    fix specs to work with tmp root page

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 describe "layout application", :type => :feature do
+  # TMP Fix, as long as there's no root layout
+  before :each { @campaign = FactoryBot.create :campaign }
+
   it "shows the about page" do
-    visit root_path
+    #visit root_path
+    visit campaign_path(@campaign)
     find('.qa-about-us a').click
     expect(page.status_code).to eq(200)
     expect(current_path).to eq(static_about_us_path)
   end
 
   it "shows the tos page" do
-    visit root_path
+    #visit root_path
+    visit campaign_path(@campaign)
     find('.qa-terms-of-service a').click
     expect(page.status_code).to eq(200)
     expect(current_path).to eq(static_terms_of_service_path)
