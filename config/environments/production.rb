a7c0e9aa3d88231c5e2d8cdd76038066be1f7040
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-11-05 20:30:06 +0100

    Update ActionMailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'rails@swiss-crowdfunder.net'}
+
   config.middleware.use ExceptionNotification::Rack,
     email: {
                deliver_with: :deliver,
