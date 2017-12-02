8b01a2df146bf5318c7e56f5a295b9ac4844e72a
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-12-02 22:59:56 +0100

    Configure Staging and Production servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'swiss-crowdfunding.net', user: 'app', roles: %w{web app db}
+server 'swiss-crowdfunder.com', user: 'app', roles: %w{web app db}
 set :branch, ENV['REVISION'] || 'master'
 
 
