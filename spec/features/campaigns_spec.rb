0c0b593ea04c379f6c1d8f37a43a33ce67a57ab3
Alain M. Lafon <alain@200ok.ch>
2018-09-30 14:43:27 +0200

    refactor: campaigns_spec.rb follows style-guide

--- spec/features/campaigns_spec.rb
+++ spec/features/campaigns_spec.rb
@@ -8,12 +8,17 @@ describe 'campaigns' do
       start_date: 100.days.from_now,
       end_date: 200.days.from_now
 
-    @goody1 = FactoryBot.create :goody, title: 'Spec Goody 1', campaign: @campaign, quantity: 1
-    @goody2 = FactoryBot.create :goody, title: 'Spec Goody 2', campaign: @campaign, quantity: 0
+    @goody1 = FactoryBot.create :goody,
+      title: 'Spec Goody 1',
+      campaign: @campaign,
+      quantity: 1
+    @goody2 = FactoryBot.create :goody,
+      title: 'Spec Goody 2',
+      campaign: @campaign,
+      quantity: 0
   end
 
   feature 'campaigns#show' do
-
     scenario 'renders' do
       visit campaign_path(@campaign)
       expect(page).to have_content 'Spec Campaign'
@@ -26,51 +31,48 @@ describe 'campaigns' do
   end
 
   feature 'date based logic' do
-
     describe 'before the campaign starts' do
-
       scenario 'shows a human readable starts until time' do
         visit campaign_path(@campaign)
-        expect(page).to have_content "Noch 3 Monate bis zum Start!"
-        expect(page).to have_css ".qa-time_until_start"
+        expect(page).to have_content 'Noch 3 Monate bis zum Start!'
+        expect(page).to have_css '.qa-time_until_start'
       end
 
       scenario 'has a disabled support button', js: true do
         visit campaign_path(@campaign)
-        expect(page).to have_css(".qa-support-project.disabled")
+        expect(page).to have_css('.qa-support-project.disabled')
 
         expect do
-          find(".qa-support-project").click
+          find('.qa-support-project').click
         end.to raise_error(Selenium::WebDriver::Error::ElementClickInterceptedError)
       end
 
       scenario 'has diabled pledge buttons for goodies', js: true do
         visit campaign_path(@campaign)
-        expect(page).to have_css(".qa-pledge.disabled")
+        expect(page).to have_css('.qa-pledge.disabled')
 
         expect do
-          first(".qa-pledge").click
+          first('.qa-pledge').click
         end.to raise_error(Selenium::WebDriver::Error::ElementClickInterceptedError)
       end
     end
 
     describe 'while the campaign runs' do
-
       scenario 'does not show a human readble starts until time' do
         Timecop.freeze(Date.today + 100) do
           visit campaign_path(@campaign)
         end
-        expect(page).to_not have_css ".qa-time_until_start"
+        expect(page).to_not have_css '.qa-time_until_start'
       end
 
       scenario 'has a working pledge button for goodies', js: true do
         Timecop.freeze(Date.today + 100) do
           visit campaign_path(@campaign)
         end
-        expect(page).to have_css(".qa-pledge.disabled")
+        expect(page).to have_css('.qa-pledge.disabled')
 
         expect do
-          first(".qa-pledge").click
+          first('.qa-pledge').click
         end.to_not raise_error
       end
     end
@@ -83,12 +85,11 @@ describe 'campaigns' do
   end
 
   feature 'buying goodies' do
-
     scenario 'confirmation page' do
       visit campaign_goodies_path([@campaign])
       first('.qa-pledge').click
       # Text from @campaign.order_description
-      expect(page).to have_content("How to buy")
+      expect(page).to have_content('How to buy')
       expect(page).to have_content I18n.t('orders.new.confirmation')
     end
 
@@ -111,40 +112,37 @@ describe 'campaigns' do
       expect do
         Timecop.freeze(Date.today + 100) do
           visit campaign_goodies_path([@campaign])
-          expect(first('.qa-pledge')).to_not have_css("a.disabled")
+          expect(first('.qa-pledge')).to_not have_css('a.disabled')
           first('.qa-pledge').click
 
-          fill_in I18n.t('orders.new.first_name'), with: "John"
-          fill_in I18n.t('orders.new.last_name'), with: "Doe"
-          fill_in I18n.t('orders.new.email'), with: "supporter@example.com"
-          fill_in I18n.t('orders.new.street'), with: "Spec Street"
-          fill_in I18n.t('orders.new.postal_code'), with: "12345"
-          fill_in I18n.t('orders.new.city'), with: "Glarus"
-          select "Schweiz", from: I18n.t('orders.new.country')
-          select "1950", from: "order_supporter_attributes_date_of_birth_1i"
+          fill_in I18n.t('orders.new.first_name'), with: 'John'
+          fill_in I18n.t('orders.new.last_name'), with: 'Doe'
+          fill_in I18n.t('orders.new.email'), with: 'supporter@example.com'
+          fill_in I18n.t('orders.new.street'), with: 'Spec Street'
+          fill_in I18n.t('orders.new.postal_code'), with: '12345'
+          fill_in I18n.t('orders.new.city'), with: 'Glarus'
+          select 'Schweiz', from: I18n.t('orders.new.country')
+          select '1950', from: 'order_supporter_attributes_date_of_birth_1i'
           find('.qa-agreement').click
           find('.qa-submit').click
           expect(page).to_not have_content I18n.t('errors.messages.blank')
           # Text from @campaign.order_success
           expect(page).to have_content('We will get back to you')
         end
-      end.to change{Order.count}.from(0).to(1)
+      end.to change { Order.count }.from(0).to(1)
     end
-
 
     scenario 'with agreement, enough quantity and all fields filled out' do
       expect do
         visit campaign_goodies_path([@campaign])
         second('.qa-pledge').click
         expect(current_path).to be(campaign_goodies_path([@campaign]))
-        expect(second('.qa-pledge')).to have_css("a.disabled")
+        expect(second('.qa-pledge')).to have_css('a.disabled')
       end
     end
   end
 
-
-  describe "FriendlyId" do
-
+  describe 'FriendlyId' do
     it 'works on newly created resources' do
       campaign = FactoryBot.create :campaign, title: 'spec title'
       expect(campaign.slug).to eq('spec-title')
