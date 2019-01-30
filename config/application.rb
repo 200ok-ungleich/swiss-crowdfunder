67d7a6f71074fc7faff9657ff4b6dfaacbd94631
Malachai Frazier <malachaifrazier@users.noreply.github.com>
2019-01-30 13:44:58 -0500

    ran  app:update

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,5 @@ module Crowdfunding
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.exceptions_app = self.routes
   end
 end
