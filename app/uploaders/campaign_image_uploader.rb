cfb33e7ae61b667bad300cc443149a61ea271e57
Alain M. Lafon <alain@200ok.ch>
2019-03-13 21:54:43 +0100

    fix: failing tests after Rails upgrade

--- app/uploaders/campaign_image_uploader.rb
+++ app/uploaders/campaign_image_uploader.rb
@@ -15,6 +15,10 @@ class CampaignImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url()
+    '/android-chrome-512x512.png'
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
